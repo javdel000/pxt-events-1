db82989865b7d6949281d597d69a776541e4f7e7
javdel000 <58610616+javdel000@users.noreply.github.com>
2019-12-12 13:44:58 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+let _1100 = 0
+let randomNumber15 = Math.randomRange(1, 5)
+game.splash("This is a number 1-5" + ":" + randomNumber15, "")
+let _610 = Math.randomRange(6, 10)
+game.splash("This is a number 6-10" + ":" + _610, "")
+let _1120 = Math.randomRange(11, 20)
+game.splash("This is a number 11-20" + ":" + _1120, "")
+_610 = Math.randomRange(1, 100)
+game.splash("This is a number 1-100" + ":" + _1100, "")
